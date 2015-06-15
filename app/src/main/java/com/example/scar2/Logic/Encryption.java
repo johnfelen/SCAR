6a9b992cd397d7fff186d32889ce510a0ff51d61
jtf28 <jtf28@pitt.edu>
2015-06-15 19:20:51 -0400

    Encryption should be finished but I am throwing an exception(seems like I must do it) and have two large try catch blocks for code that have worked numerous times outside of android studio

--- app/src/main/java/com/example/scar2/Logic/Encryption.java
+++ app/src/main/java/com/example/scar2/Logic/Encryption.java
@@ -16,32 +16,48 @@ public class Encryption {
     }
 
     Cipher cipher;	//cipher to encryp/decrypt
-    MessageDigest sha256 = MessageDigest.getInstance("SHA-256", "SC");   //used to hash the key to get 256-bit sha
 
-    public byte[] encrypt(byte[] data, String stringOfKey)  throws Exception    //takes the original data and transforms it into cipher text*/
+    public byte[] encrypt(byte[] data, String stringOfKey)  //takes the original data and transforms it into cipher text, NOTE: There are a lot of exceptions bu I think it is android, I have not run this code in android yet
     {
-        //set up
-        SecretKey AESkey = getAESKey(stringOfKey);
-        cipher = Cipher.getInstance("AES", "SC");   //set up cipher for AES
-        cipher.init( Cipher.ENCRYPT_MODE, AESkey );	//set the cipher as encrypt mode with the key
+        try
+        {
+            //set up
+            SecretKey AESkey = getAESKey(stringOfKey);
+            cipher = Cipher.getInstance("AES", "SC");   //set up cipher for AES
+            cipher.init(Cipher.ENCRYPT_MODE, AESkey);    //set the cipher as encrypt mode with the key
 
-        byte[] cipherText = cipher.doFinal(data);         //the actual encryption
-        return Base64.encode(cipherText);   //return the Base64 encrypted version of the cipher text, Base64 is for padding errors
+            byte[] cipherText = cipher.doFinal(data);         //the actual encryption
+            return Base64.encode(cipherText);   //return the Base64 encrypted version of the cipher text, Base64 is for padding errors
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+            return null;
+        }
     }
 
-    public byte[] decrypt(byte[] cipherText, String stringOfKey)    throws Exception    //takes the cipherText and returns the original data*/
+    public byte[] decrypt(byte[] cipherText, String stringOfKey)    //takes the cipherText and returns the original data, NOTE: same as encrypt method
     {
-        //set up
-        SecretKey AESkey = getAESKey( stringOfKey );
-        cipher = Cipher.getInstance("AES", "SC");   //set up cipher for AES
-        cipher.init( Cipher.DECRYPT_MODE, AESkey );	//set the cipher as encrypt mode with the key
+        try
+        {
+            //set up
+            SecretKey AESkey = getAESKey(stringOfKey);
+            cipher = Cipher.getInstance("AES", "SC");   //set up cipher for AES
+            cipher.init(Cipher.DECRYPT_MODE, AESkey);    //set the cipher as encrypt mode with the key
 
-        cipherText = Base64.decode(cipherText); //remove the Base64 buffer
-        return cipher.doFinal( cipherText );    //returns the original data
+            cipherText = Base64.decode(cipherText); //remove the Base64 buffer
+            return cipher.doFinal(cipherText);    //returns the original data
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+            return null;
+        }
     }
 
-    private SecretKey getAESKey( String stringOfKey )  //takes the stringOfKey and transforms it into a usuable 256-bit AES key
+    private SecretKey getAESKey( String stringOfKey )  throws NoSuchAlgorithmException  //takes the stringOfKey and transforms it into a usuable 256-bit AES key, NOTE: From reading online, I am supposed to through this exception
     {
+        MessageDigest sha256 = MessageDigest.getInstance("SHA-256");   //used to hash the key to get 256-bit sha
         SecretKey AESkey;	//AES key used for cipher
         sha256.update( stringOfKey.getBytes() );   //perform the hash on the stringOfKey
         AESkey = new SecretKeySpec( sha256.digest(), "AES" );  //creates AESkey from the stringOfKey
