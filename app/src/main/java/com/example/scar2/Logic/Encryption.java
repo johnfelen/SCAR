233d7b294d4880e3700a97530d5f53aa42551bdd
jtf28 <jtf28@pitt.edu>
2015-06-15 19:09:05 -0400

    spongy castle added, still getting a weird error with MessageDigest

--- app/src/main/java/com/example/scar2/Logic/Encryption.java
+++ app/src/main/java/com/example/scar2/Logic/Encryption.java
@@ -2,37 +2,41 @@ package com.example.scar2.Logic;
 
 import java.security.*;
 import javax.crypto.*;
-//import org.bouncycastle.jce.provider.BouncyCastleProvider;    TODO:fix this line
-//import org.bouncycastle.util.encoders.Base64;
+import org.spongycastle.*;
+import org.spongycastle.util.encoders.Base64;
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
-import org.apache.commons.codec.binary.Base64;
 
 public class Encryption {
 
-    //Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());	//add bouncy castle as a provider   TODO:fix this line
+    /*This static statement must be put into any file using spongy castle crypto so that Android knows to use the spongy castle jars*/
+    static
+    {
+        Security.insertProviderAt(new org.spongycastle.jce.provider.BouncyCastleProvider(), 1);
+    }
+
     Cipher cipher;	//cipher to encryp/decrypt
-    MessageDigest sha256 = MessageDigest.getInstance("SHA-256");   //used to hash the key to get 256-bit sha
+    MessageDigest sha256 = MessageDigest.getInstance("SHA-256", "SC");   //used to hash the key to get 256-bit sha
 
     public byte[] encrypt(byte[] data, String stringOfKey)  throws Exception    //takes the original data and transforms it into cipher text*/
     {
         //set up
-        SecretKey AESkey = getAESKey( stringOfKey );
-        cipher = Cipher.getInstance("AES", "BC");   //set up cipher for AES
+        SecretKey AESkey = getAESKey(stringOfKey);
+        cipher = Cipher.getInstance("AES", "SC");   //set up cipher for AES
         cipher.init( Cipher.ENCRYPT_MODE, AESkey );	//set the cipher as encrypt mode with the key
 
         byte[] cipherText = cipher.doFinal(data);         //the actual encryption
-        return Base64.encodeBase64( cipherText );   //return the Base64 encrypted version of the cipher text, Base64 is for padding errors
+        return Base64.encode(cipherText);   //return the Base64 encrypted version of the cipher text, Base64 is for padding errors
     }
 
     public byte[] decrypt(byte[] cipherText, String stringOfKey)    throws Exception    //takes the cipherText and returns the original data*/
     {
         //set up
         SecretKey AESkey = getAESKey( stringOfKey );
-        cipher = Cipher.getInstance("AES", "BC");   //set up cipher for AES
+        cipher = Cipher.getInstance("AES", "SC");   //set up cipher for AES
         cipher.init( Cipher.DECRYPT_MODE, AESkey );	//set the cipher as encrypt mode with the key
 
-        cipherText = Base64.decodeBase64( cipherText ); //remove the Base64 buffer
+        cipherText = Base64.decode(cipherText); //remove the Base64 buffer
         return cipher.doFinal( cipherText );    //returns the original data
     }
 
