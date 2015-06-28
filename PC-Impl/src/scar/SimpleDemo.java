08a079dab91c14a07e5d3e58e703feb2e42bed3c
Aytros <redsax20@gmail.com>
2015-06-28 11:22:13 -0400

    filling out SimpleDemo.java

--- PC-Impl/src/scar/SimpleDemo.java
+++ PC-Impl/src/scar/SimpleDemo.java
@@ -2,16 +2,38 @@ package scar.pc;
 
 public class SimpleDemo {
   public static void get(String fn, String password, int k, int n) {
-  
+
+    //like StoreFile, GetFile takes a int buffer.  where is this coming from?
+
+    GetFile gf = new GetFile(fn, password, k, n);
+    gf.get();
+
   }
 
   public static void store(String path, String fn, String password, int k, int n) {
-    
+
+    //SToreFile.java's constructor takes more parameters than this function takes it
+    //like the byte[], int buffer, and the IServers[]??  Create them here?
+
+    StoreFile sf = new StoreFile(fn, password, k, n);
+    sf.store();
+
   }
   
   //Usage: SimpleDemo -get FILENAME PASSWORD K N
   //                  -store FILE-PATH FILENAME PASSWORD K N
   public static void main(String args[]) {
-    
+
+    ///using place holders right now for the demo
+    String path;
+    String fn = "testFile";
+    String password = "securePass";
+    int k;
+    int n;
+
+    store(path, fn, password, k, n);
+    get(fn, passowrd, k, n);
+
+    //are we displaying the output for the demo?
   }
 }
