660aa2e858c09f990ef7ee2b5578f7534668d434
Corey Furmanski <furmanskic@live.com>
2015-06-30 01:33:04 -0400

    fixed any issues to get demo working, see test folder for examples

--- PC-Impl/src/scar/SimpleDemo.java
+++ PC-Impl/src/scar/SimpleDemo.java
@@ -1,60 +1,71 @@
 package scar.pc;
 
+import java.io.*;
+
 public class SimpleDemo {
-  public static void get(String fn, String password, int k, int n) {
-    GetFile gf = new GetFile(fn, password, k, n);
+  public static void get(String path, String fn, String password, int k, int n) {
+    scar.IServer servers[] = { new scar.LocalStore("encoded") };
+    scar.GetFile gf = new scar.GetFile(fn, password, k, n, servers);
 
     byte[] data = gf.get(); //data => file bytes
-    //Make a new file with filename 'fn'
+    //Make a new file with filename 'path'
     //Store the data bytes via an FileOutputStream.write for every byte into the file
-    File file = new File(fn);
-    file.createNewFile();
-    FileOutputStream fout = new FileOUtputStream(file);
-    fout.write(data);
-
+    try {
+      File file = new File(path);
+      file.createNewFile();
+      FileOutputStream fout = new FileOutputStream(file);
+      fout.write(data);
+      fout.flush();
+      fout.close();
+    } catch(Exception e) {
+      e.printStackTrace();
+      System.out.println("Failed to save file: " + path);
+    }
   }
 
   public static void store(String path, String fn, String password, int k, int n) {
-    File file = new File(fn);
-    FileInputStream fin = new FileInputStream(file);
-    filesize = file.length();
-    byte[] bArr = new byte[filesize];
-
-    //Read in the file bArr[0...len] = fin.read();
-    fin.read(bArr);
-
-    StoreFile sf = new StoreFile(bArr, fn, password, k, n);
+    byte[] bArr = null;
+    try {
+      File file = new File(path);
+      FileInputStream fin = new FileInputStream(file);
+      int filesize = (int)file.length();
+      bArr = new byte[filesize];
+      
+      //Read in the file bArr[0...len] = fin.read();
+      fin.read(bArr);
+      fin.close();
+    } catch(Exception e) {
+      e.printStackTrace();
+      System.out.println("Can't load file: " + fn);
+    }
+      
+    scar.IServer servers[] = { new scar.LocalStore("encoded") };
+    scar.StoreFile sf = new scar.StoreFile(bArr, fn, password, k, n, servers);
     sf.store();
   }
   
-  //Usage: SimpleDemo -get FILENAME PASSWORD K N
+  //Usage: SimpleDemo -get STORE-PATH FILENAME PASSWORD K N
   //                  -store FILE-PATH FILENAME PASSWORD K N
   public static void main(String args[]) {
 
-    String path;
-    String fn;
-    String password;
-    int k;
-    int n;
+    String path = args[1];
+    String fn = args[2];
+    String password = args[3];
+    int k = Integer.parseInt(args[4]);
+    int n = Integer.parseInt(args[5]);
+    
+    scar.Encryption.setEncryption(PCEncrypt.class);
 
     if(args[0].equals("-store")) {
-      path = args[1];
-      fn = args[2];
-      password = args[3];
-      k = args[4];
-      n = args[5];
       store(path, fn, password, k, n);
     }
     else if(args[0].equals("-get")) {
-      fn = args[1];
-      password = args[2];
-      k = args[3];
-      n = args[4];
-      get(fn, password, k, n);
+      get(path, fn, password, k, n);
     }
     else {
       //invalid usage
-      System.out.println("Invalid usage!")
+      System.out.println("Usage: java -jar scar-pc.jar -get STORE-FILE FILENAME PASSWORD K N");
+      System.out.println("                             -store FILE-PATH FILENAME PASSWORD K N");
     }
   }
 }
---
+++ PC-Impl/test/examples.bat
@@ -0,0 +1,2 @@
+java -jar scar-pc.jar -store bcprov-jdk15on-152.jar crypt test9999 100 200
+java -jar scar-pc.jar -get bcprov.jar crypt test9999 100 200
