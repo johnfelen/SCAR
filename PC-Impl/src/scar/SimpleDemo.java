8bfd1b1bc4155494affc10db41279aa9d02eb25b
Corey Furmanski <furmanskic@live.com>
2015-06-29 20:30:50 -0400

    SimpleDemo was left out of last commit

--- PC-Impl/src/scar/SimpleDemo.java
+++ PC-Impl/src/scar/SimpleDemo.java
@@ -1,38 +1,42 @@
 package scar.pc;
 
 public class SimpleDemo {
-  public static void get(String fn, String password, int k, int n) {
-    //not sure of the interplay here....
+  public static void get(String fn, String password, int k, int n) {   
     GetFile gf = new GetFile(fn, password, k, n);
-    gf.get();
-
+    
+    byte[] data = gf.get(); //data => file bytes
+    //Make a new file with filename 'fn'
+    //Store the data bytes via an FileOutputStream.write for every byte into the file
   }
 
   public static void store(String path, String fn, String password, int k, int n) {
-    
     FileInputStream fin = new FileInputStream(new File(fn));
-    byte[] bArr = new byte[n];  //assuming byte[] is length n
-    fin.read();  //not sure which implementation would be better
+    //filesize = File.length();
+    byte[] bArr = new byte[(new File(fn)).length()]; 
+    
+    //Read in the file bArr[0...len] = fin.read();
 
     StoreFile sf = new StoreFile(bArr, fn, password, k, n);
     sf.store();
-
   }
   
   //Usage: SimpleDemo -get FILENAME PASSWORD K N
   //                  -store FILE-PATH FILENAME PASSWORD K N
   public static void main(String args[]) {
-
-    ///using place holders right now for the demo
+    
+    //Assign these via their approriate args[index];
     String path;
     String fn;
     String password;
     int k;
     int n;
 
-    store(path, fn, password, k, n);
-    get(fn, password, k, n);
-
-    //are we displaying the output for the demo?
+    if(args[0].equals("-store"))
+      store(path, fn, password, k, n);
+    else if(args[0].equals("-get"))
+      get(fn, password, k, n);
+    else {
+      //invalid usage
+    }
   }
 }
