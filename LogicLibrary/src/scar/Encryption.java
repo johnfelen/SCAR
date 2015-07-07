a49115b969259c4230b67fd7048248d081f6330d
jtf28 <jtf28@users.noreply.github.com>
2015-07-07 18:07:28 -0400

    Update Encryption.java

--- LogicLibrary/src/scar/Encryption.java
+++ LogicLibrary/src/scar/Encryption.java
@@ -3,31 +3,10 @@ package scar;
 import java.lang.reflect.*;
 
 public class Encryption {
-  private static Constructor<Encryption> factory; 
-  
-  //Set the encryption class to be used in the implementation
-  public static void setEncryption(Class<? extends Encryption> eclass) {
-    try {
-      factory = (Constructor<Encryption>)eclass.getConstructor();
-    } catch(Exception e) {
-      e.printStackTrace();
-      System.exit(1);
-    }
-  }
-
-  
   //Use this to get an object instance of the encryption class 
   //used in the implementation
   public static Encryption getInstance() {
-    if(factory != null) {
-      try {
-        return factory.newInstance();
-      } catch(Exception e) {
-        e.printStackTrace();
-        return null;
-      }
-    }
-    return null;
+    return new Encryption();
   }
 
   public Encryption() {} 
