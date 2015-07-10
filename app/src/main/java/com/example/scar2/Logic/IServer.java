45bc0711cddcc05f6fabda10c934d0f56d8fae7d
Corey Furmanski <caf98@pitt.edu>
2015-07-09 23:17:11 -0400

    removed some unused files, updated gitignore and a few builds

--- app/src/main/java/com/example/scar2/Logic/IServer.java
+++
@@ -1,8 +0,0 @@
-package com.example.scar2.Logic;
-
-//Abstract away the actual storage server implementation/driver.
-//MySQL is an example of a storage server that will implement this eventually. 
-public interface IServer {
-  public void storeData(String fn, byte[] data);
-  public byte[] getData(String fn);
-}
