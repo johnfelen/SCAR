5f0fca4650b951d322e0a60a679bbf6c52b5f3d5
jtf28 <jtf28@pitt.edu>
2015-07-21 20:34:18 -0400

    create password alertbox mostly finished

--- app/src/main/java/com/scar/android/Activities/LoginActivity.java
+++ app/src/main/java/com/scar/android/Activities/LoginActivity.java
@@ -1,11 +1,14 @@
 package com.scar.android.Activities;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 
 import com.scar.android.MetaData;
 import com.android.scar.R;
@@ -16,6 +19,8 @@ import com.scar.android.Session;
  */
 public class LoginActivity extends Activity
 {
+    String newPassword; //a newly created password will be stored here
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -43,14 +48,34 @@ public class LoginActivity extends Activity
                 }
             }
         });
+
         create.setOnClickListener(new Button.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                //Start create password activity
+            public void onClick(View v) {   //Start create password activity
+                    /*Creates the alertdialog with title and message*/
+                    AlertDialog.Builder createNewPass = new AlertDialog.Builder(LoginActivity.this);
+                    createNewPass.setTitle("Create Password");
+                    createNewPass.setMessage("A password must be at least 32 characters long.");
+
+                    /*This input EditText will be where they actually enter a password*/
+                    final EditText input = new EditText(LoginActivity.this);
+                    LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                            LinearLayout.LayoutParams.MATCH_PARENT,
+                            LinearLayout.LayoutParams.MATCH_PARENT);
+                    input.setLayoutParams(lp);
+                    createNewPass.setView(input);
+
+                    /*This onclicklistenere will check if the password is atleast 32 characters and then store it into newPassword*/
+                    createNewPass.setPositiveButton("CREATE", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int whichButton) {
+                            newPassword = input.getText().toString();
+                        }
+                    });
+                    createNewPass.setNegativeButton("CANCEL", null);
+                    createNewPass.create().show();
             }
         });
-
-		/*Login part*/
 
     }
 
