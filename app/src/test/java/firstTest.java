cad210e8ae89042cf5b962e1d274a96c6c0cf80a
lmn33 <lmn33@pitt.edu>
2016-04-11 07:44:44 -0400

    Starting test Cases

--- app/src/test/java/firstTest.java
+++ app/src/test/java/firstTest.java
@@ -1,3 +1,8 @@
+import android.content.Context;
+
+import com.scar.android.Lock;
+import com.scar.android.Session;
+
 import static org.junit.Assert.*;
 import org.junit.Test;
 //import org.junit.Test;
@@ -7,11 +12,17 @@ import static org.mockito.Mockito.*;
  * Created by Luke on 4/11/2016.
  */
 public class firstTest {
+
+    //Ensures the wait time of the lock is five minutes
+    Context context;
     @Test
-    public void testVerifyWorks()
+    public void testSession()
     {
+        //context = getInstrumentation().getContext();
         // create and configure mock
-        boolean works=true;
-        assertTrue(works);
+        Session newSession=new Session();
+        Session.makeLock(context);
+        long waitTime=newSession.remaining();
+        assertEquals(waitTime,300000);
     }
 }
