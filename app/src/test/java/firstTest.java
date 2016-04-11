7c05b0209eddc227e8821c47445190a22ec7e9f0
lmn33 <lmn33@pitt.edu>
2016-04-11 07:24:33 -0400

    JUnit tests Work :dancers: :dancers:

---
+++ app/src/test/java/firstTest.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+//import org.junit.Test;
+import static org.mockito.Mockito.*;
+
+/**
+ * Created by Luke on 4/11/2016.
+ */
+public class firstTest {
+    @Test
+    public void testVerifyWorks()
+    {
+        // create and configure mock
+        boolean works=true;
+        assertTrue(works);
+    }
+}
