7ceec476628195207b41a64eb51a5ec3e5030b0c
lmn33 <lmn33@pitt.edu>
2016-04-15 10:11:19 -0400

    Scar File Tests

---
+++ app/src/test/java/Tests/ScarFileTest.java
@@ -0,0 +1,54 @@
+package Tests;
+
+import com.scar.android.ScarFile;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Luke on 4/15/2016.
+ */
+public class ScarFileTest {
+    @Test
+    public void ScarTest()
+    {
+        int id=1;
+        String name="Scar";
+        ScarFile newScar=new ScarFile(id,name);
+        String tester=newScar.getFilename();
+        assertTrue(tester.equals(name));
+    }
+
+    public void setLocalPathsTest()
+    {
+        String tester1="s";
+        String tester2="New";
+        String tester3="Brand";
+        int id=1;
+        String name="Scar";
+        ScarFile newScar=new ScarFile(id,name);
+        newScar.addLocal(tester1);
+        newScar.addLocal(tester2);
+        newScar.addLocal(tester3);
+        ArrayList<String> scarArray=newScar.getLocalpaths();
+        for(int i=0;i<scarArray.size();++i)
+        {
+            if(i==0)
+            {
+                assertTrue(scarArray.get(i).equals(tester1));
+            }
+            else if(i==1)
+            {
+                assertTrue(scarArray.get(i).equals(tester2));
+            }
+            else if(i==2)
+            {
+                assertTrue(scarArray.get(i).equals(tester3));
+            }
+        }
+
+    }
+}
